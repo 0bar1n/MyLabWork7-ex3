82a403382fe95a06f2d095fe32ab70d450a95850
barinskaya <1388zxc@gmail.com>
2022-10-14 15:05:15 +0300

    Добавьте файлы проекта.

---
+++ MyLabWork7 ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyLabWork7_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r,h,d; //переменные вещественного типа
+            r = 6350; //задние переменной радиуса из условия
+            Console.WriteLine("Высота над Землей h="); //просим пользователя ввести высоту над Землей
+            h = Convert.ToDouble(Console.ReadLine()); //задание переменной вещественного типа
+            d = Math.Sqrt(h*(2*r+h)); // задание переменной по формуле, нахождение расстояния до линии горизонта
+            Console.WriteLine($"Расстояние до линии горизонта равно d = {d}км" ); //вывод на экран 
+        }
+    }
+}
